992493507061caf66e5a1c2885489e0f3b7ac438
a <a@b.com>
2020-04-05 18:04:44 +0530

    added tests

--- maven-demo/src/main/java/com/br/training/Application.java
+++ maven-demo/src/main/java/com/br/training/Application.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+		
+		public int countWords(String word) {
+			String[] split1 = StringUtils.split(word,' ');
+			return (split1 == null ? 0: split1.length);
+		}
+		
 		public static void main(String args[]) {
 			System.out.println("Hello !!");
 			List<String> greetings = new ArrayList<String>();

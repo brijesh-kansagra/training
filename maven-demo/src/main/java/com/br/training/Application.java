3b58de5d54c0a7662187943ae028b1e938b2dd26
a <a@b.com>
2020-04-05 17:26:39 +0530

    added java 8

--- maven-demo/src/main/java/com/br/training/Application.java
+++ maven-demo/src/main/java/com/br/training/Application.java
@@ -1,7 +1,11 @@
 package com.br.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
 		public static void main(String args[]) {
 			System.out.println("Hello !!");
+			List<String> greetings = new ArrayList<String>();
 		}
 }

52c1a2e87b889f46c1919a778175cc6ec56ee815
a <a@b.com>
2020-04-05 17:39:19 +0530

    added dependency

--- maven-demo/src/main/java/com/br/training/Application.java
+++ maven-demo/src/main/java/com/br/training/Application.java
@@ -2,10 +2,13 @@ package com.br.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 		public static void main(String args[]) {
 			System.out.println("Hello !!");
 			List<String> greetings = new ArrayList<String>();
+			System.out.println("Split Count : "+StringUtils.split("abc def",' ').length);
+			
 		}
 }
